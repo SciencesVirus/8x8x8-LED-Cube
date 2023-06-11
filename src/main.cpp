1c029fdc7aad64dc1213b78e75c428005ed43fe6
John L <john.lukito@gmail.com>
2023-06-11 23:13:40 +0700

    feat: working firebase?

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,53 @@
 #include <Arduino.h>
 #include <SPI.h>
 
+#include <WiFi.h>
+#include <Firebase.h>
+#include <credentials.h>
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
+FirebaseData stream;
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+//--------- FIREBASE CALLBACKS ---------//
+
+void streamCallback(StreamData data) {
+  noInterrupts();
+  Serial.printf("stream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\n\n",
+                data.streamPath().c_str(),
+                data.dataPath().c_str(),
+                data.dataType().c_str(),
+                data.eventType().c_str());
+  printResult(data); // see addons/RTDBHelper.h
+  Serial.println();
+
+  if (data.boolData() == true) {
+    digitalWrite(BUILTIN_LED, HIGH);
+  } else {
+    digitalWrite (BUILTIN_LED, LOW);
+  }
+  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", data.payloadLength(), data.maxPayloadLength());
+  interrupts();
+}
+
+void streamTimeoutCallback(bool timeout) {
+  if (timeout)
+    Serial.println("stream timed out, resuming...\n");
+
+  if (!stream.httpConnected())
+    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
+}
+
+//--------- FIREBASE CALLBACKS ---------//
+
 #define latch_pin 4
 #define blank_pin 22
 #define data_pin 23
@@ -1660,6 +1707,9 @@ void harlem_shake() {
 
 
 void setup() {
+
+  Serial.begin (115200); // DEBUG ONLY
+
   noInterrupts();
   SPI.setBitOrder(MSBFIRST);
   SPI.setDataMode(SPI_MODE0);
@@ -1678,6 +1728,7 @@ void setup() {
   pinMode(data_pin, OUTPUT);
   pinMode(clock_pin, OUTPUT);
   pinMode(blank_pin, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
 
   // set blank high
   digitalWrite(blank_pin, HIGH);
@@ -1689,40 +1740,53 @@ void setup() {
 
   SPI.begin();
   interrupts();
+
+
+  // WIFI AND FIREBASE
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  Firebase.begin(&config, &auth);
+
+  Firebase.reconnectWiFi(true);
+
+  if (!Firebase.beginStream(stream, "/ledState/on"))
+    Serial.printf("sream begin error, %s\n\n", stream.errorReason().c_str());
+
+  Firebase.setStreamCallback(stream, streamCallback, streamTimeoutCallback);
+  // WIFI AND FIREBASE
+
+
+  //SETTINGS//
   // Bright : 800 - 100 : Flicker-free
   My_timer = timerBegin(0, 800, true);
   timerAttachInterrupt(My_timer, &onTimer, true);
-  timerAlarmWrite(My_timer, 175, true);
+  timerAlarmWrite(My_timer, 175, true); // DO NOT CHANGE
   timerAlarmEnable(My_timer);
 }
-
-
-
-void letterMrow4(int row) {
-  LED(0, row, 0, 0, 0, 15);
-  LED(1, row, 0, 0, 0, 15);
-  LED(2, row, 0, 0, 0, 15);
-  LED(3, row, 0, 0, 0, 15);
-  LED(4, row, 0, 0, 0, 15);
-  LED(5, row, 0, 0, 0, 15);
-  LED(6, row, 0, 0, 0, 15);
-  LED(7, row, 0, 0, 0, 15);
-  LED(1, row, 1, 0, 0, 15);
-  LED(2, row, 2, 0, 0, 15);
-  LED(3, row, 3, 0, 0, 15);
-  LED(3, row, 4, 0, 0, 15);
-  LED(2, row, 5, 0, 0, 15);
-  LED(1, row, 6, 0, 0, 15);
-  LED(0, row, 7, 0, 0, 15);
-  LED(1, row, 7, 0, 0, 15);
-  LED(2, row, 7, 0, 0, 15);
-  LED(3, row, 7, 0, 0, 15);
-  LED(4, row, 7, 0, 0, 15);
-  LED(5, row, 7, 0, 0, 15);
-  LED(6, row, 7, 0, 0, 15);
-  LED(7, row, 7, 0, 0, 15);
-}
-
 
 void loop() {
   rainVersionTwo();
